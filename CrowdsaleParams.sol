8d9af6b057abd1f719cf820c5a6b8aa9c8416aa7
mixzenin <mix.zenin.ifmo@yandex.ru>
2018-03-24 20:32:25 +0300

    fixed panel price

--- CrowdsaleParams.sol
+++ CrowdsaleParams.sol
@@ -4,6 +4,5 @@ contract CrowdsaleParams {
   uint public constant  REMAINED_BOUNTY_TOKENS = 1893000;
   uint public constant PRICE_ETH_USD = 120000;// cent
   uint public constant  SOFTCAP = 100;// general
-  uint public constant PANEL_PRICE = 600; // in tokens
-  uint public constant TOKEN_BUYOUT_PRICE = 110; // in cent
+  uint public constant PANEL_PRICE = 600 * (10 ** 18); // in tokens with decimals
 }
