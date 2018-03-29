e8368b3c6e1ac9e0ad5f145c1badaa923fc28e08
mixzenin <mix.zenin.ifmo@yandex.ru>
2018-03-29 20:43:16 +0300

    added comments

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -6,6 +6,9 @@ import "./ICOParams.sol";
 import "./CrowdsaleParams.sol";
 import "./StubToken.sol";
 
+/// @title Solar Token ICO.
+/// @author Borovik Valdimir, Zenin Mikhail
+
 contract CrowdsaleStage is Access{
     using SafeMath for uint;
     uint internal startTime;
@@ -24,21 +27,30 @@ contract CrowdsaleStage is Access{
         uint remainedTokens;
     }
 
+    /// @dev Getting start time of stage
     function getStartTime() public constant onlyOwner returns (uint) {
         return startTime;
     }
 
+    /// @dev Getting end time of stage
     function getEndTime() public constant onlyOwner returns (uint) {
         return endTime;
     }
+
+    /// @dev Checking end of stage
     function getIsEnd() public constant onlyOwner returns (bool) {
         return isEnd;
     }
 
+    /// @dev Stage activity check
     function isActive() public constant onlyOwner returns (bool) {
       return (!isEnd && now >= startTime && now < endTime);
     }
 
+    /// @dev Buying tokens
+    /// @param paidWei payed wei
+    /// @param priceEthUSD price ETH in cents
+    /// @return Returns remained wei and how much tokens bought.
     function buyTokens(uint paidWei, uint priceEthUSD) public onlyOwner returns (uint remainedWei, uint tokensBought) {
       require(!getIsEnd());
       (remainedWei, tokensBought) = updateBalances(paidWei, 0, priceEthUSD);
@@ -46,6 +58,10 @@ contract CrowdsaleStage is Access{
       return (remainedWei, tokensBought);
     }
 
+    /// @dev Updating balances for each stage
+    /// @param paidWei payed wei
+    /// @param priceEthUSD price ETH in cents
+    /// @return Returns remained wei and how much user can buy tokens.
     function updateBalances(uint paidWei, uint tokensBought, uint priceEthUSD) internal returns (uint remainedWei, uint allTokensBought) {
       uint currentPrice = stages[currentStage].price;
       uint tokenWeiPrice = calculateTokenPrice(currentPrice, priceEthUSD);
@@ -67,6 +83,8 @@ contract CrowdsaleStage is Access{
       }
     }
 
+
+    /// @dev Updating current stage and remained tokens for each stage
     function updateCurrentStage() internal {
         uint8 i = 0;
         while(!(stages[i].endTime > now && stages[i].startTime <= now)) i++;
@@ -85,15 +103,23 @@ contract CrowdsaleStage is Access{
         }
     }
 
+    /// @dev Calculating token price in wei
+    /// @param centPrice price token in cents
+    /// @param priceEthUSD price ETH in cents
+    /// @return Returns price token in wei
     function calculateTokenPrice(uint centPrice, uint priceEthUSD) internal pure returns (uint weiPrice) {
         return (centPrice.mul(10 ** 18)).div(priceEthUSD);
     }
 
+    /// @dev Completion of the stage
+    /// @return Returns how much tokens was burned
     function endStage() public returns (uint burntTokens) {
         isEnd = true;
         return burnAllRemainedTokens();
     }
 
+    /// @dev Burning all remained tokens on all stages
+    /// @return Returns how much tokens was burned
     function burnAllRemainedTokens() private returns (uint burntTokens) {
         burntTokens = 0;
         for(uint8 i = 0; i < stages.length; i++) {
@@ -109,6 +135,8 @@ contract CrowdsaleStage is Access{
 
 contract PreICO is CrowdsaleStage {
   PreICOParams preIcoParams = new PreICOParams();
+
+  /// @dev Setting all default params
   function PreICO() public {
     currentStage = 0;
     startTime = preIcoParams.START_TIME();
@@ -146,6 +174,8 @@ contract PreICO is CrowdsaleStage {
 
 contract ICO is CrowdsaleStage {
   ICOParams icoParams = new ICOParams();
+
+  /// @dev Setting all default params
   function ICO() public {
     currentStage = 0;
     startTime = icoParams.START_TIME();
@@ -199,18 +229,27 @@ contract Crowdsale is SOL {
     event IcoEnded();
     event BuyPanels(address buyer, uint countPanels);
 
+    /// @dev Get remained bounty tokens
+    /// @return Returns amount of remained bounty tokens
     function getRemainedBountyTokens() public constant returns (uint){
       return remainedBountyTokens;
     }
 
+    /// @dev Get soft cap of ICO
+    /// @return Returns ICO's soft cap
     function getSoftCap() public constant returns (uint) {
       return softCap;
     }
-
+    /// @dev checking remaining tokens
+    /// @return Returns true/false, false - tokens did not end, true - no tokens
     function getOutOfTokens() public constant returns (bool) {
       return outOfTokens;
     }
 
+    /// @dev overloaded transfer function form erc20, serves for the purchase of panels
+    /// @param _to address of the recipient
+    /// @param _value amount of tokens
+    /// @return Returns true, if user want purchase panel, call transfer from erc20 if not
     function transfer(address _to, uint256 _value) public returns (bool){
       if (_to == buy_pannel) {
         buyPanel(msg.sender, _value);
@@ -219,6 +258,11 @@ contract Crowdsale is SOL {
       return super.transfer(_to, _value);
     }
 
+    /// @dev overloaded transferFrom function form erc20, serves for the purchase of panels
+    /// @param _from sender's address
+    /// @param _to address of the recipient
+    /// @param _value amount of tokens
+    /// @return Returns true, if user want purchase panel, call transferFrom from erc20 if not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
       require(_value <= balances[_from]);
       require(_value <= allowed[_from][msg.sender]);
@@ -229,14 +273,14 @@ contract Crowdsale is SOL {
       return super.transferFrom(_from, _to, _value);
     }
 
-
+    /// @dev fallback function, processes purchase of tokens
     function () public payable {
         require(msg.value > 0);
 
         if (icoStage.getIsEnd() && msg.sender == factory) return; // just save money on wallet for payout
 
         require(!outOfTokens);
-        //require(isInWhiteList(msg.sender));
+        require(isInWhiteList(msg.sender));
 
         uint paidWei;
         uint256 tokenBought;
@@ -257,7 +301,7 @@ contract Crowdsale is SOL {
         } else if (icoStage.isActive()) {
             if (!preIcoStage.getIsEnd()) {
                 preIcoStage.endStage();
-                factory.transfer(this.balance - msg.value); // send only preico money
+                factory.transfer(this.balance);
             }
             /*if (icoStage.isEnd()) {
               icoStage.endStage();
@@ -295,6 +339,7 @@ contract Crowdsale is SOL {
         }
     }
 
+    /// @dev contract constructor, initializes total supply and stages of ICO, launches presale
     function Crowdsale() public {
         totalSupply = 0;
         preIcoStage = new PreICO();
@@ -302,6 +347,7 @@ contract Crowdsale is SOL {
         preSale();
     }
 
+    /// @dev initializes presale accounts
     function preSale() internal {
       /*
       balances[0x00000] = 100;
@@ -311,26 +357,37 @@ contract Crowdsale is SOL {
       */
     }
 
+    /// @dev Adding members to white list
+    /// @param members - array of proven members
     function addMembersToWhiteList(address[] members) public onlyKyc_manager {
         for(uint i = 0; i < members.length; i++) {
             whiteList[members[i]] = true;
         }
     }
 
+    /// @dev Deleting members from white list
+    /// @param members - array of members for delete
     function deleteMembersToWhiteList(address[] members) public onlyKyc_manager {
         for(uint i = 0; i < members.length; i++) {
             whiteList[members[i]] = false;
         }
     }
 
+    /// @dev Setting ETH price in cents
+    /// @param newPrice New ETH price in cents
     function setPriceEthUSD(uint newPrice) public onlyPrice_updater { // cent
         priceEthUSD = newPrice;
     }
 
+    /// @dev Setting new token address for exchange in futher
+    /// @param _newTokenAddress new token address
     function setNewTokenAddress(address _newTokenAddress) public onlyOwner {
         newTokenAddress = _newTokenAddress;
     }
 
+    /// @dev Sending bounty tokens
+    /// @param _to address of the recipient
+    /// @param _amount amount of bounty tokens
     function sendBountyTokens(address _to, uint _amount) public onlyBounty_manager {
         require(_amount <= remainedBountyTokens);
         require(isInWhiteList(_to));
@@ -340,11 +397,15 @@ contract Crowdsale is SOL {
         totalSupply = totalSupply.add(_amount);
     }
 
+    /// @dev Finding member in whitelist
+    /// @param member member's address
+    /// @return Returns true - if member in white list, false - if not
     function isInWhiteList(address member) internal constant returns(bool){
         if(whiteList[member]) return true;
         return false;
     }
 
+    /// @dev Sending all funds to factory if preICO is end and assembled soft cap
     function sendToFactory() public onlyFactory {
       if (!preIcoStage.getIsEnd()) {
         factory.transfer(this.balance);
@@ -355,10 +416,14 @@ contract Crowdsale is SOL {
       factory.transfer(this.balance);
     }
 
+    /// @dev Getting balance of Crowdsale contract
+    /// @return Returns Crowdsale's balance
     function getBalance() public constant returns(uint) {
         return this.balance;
     }
 
+    /// @dev Returning funds to investor
+    /// @param investor investor's address
     function returnFunds(address investor) public onlyOwner {
         require(balances[investor] != 0);
         investor.transfer(weiBalances[investor]);
@@ -366,13 +431,19 @@ contract Crowdsale is SOL {
         weiBalances[investor] = 0;
     }
 
+    /// @dev Returning funds to all investors and burning all SOL tokens
     function returnAllFunds() public onlyOwner {
         for (uint i = 0; i < investors.length; i++) {
             returnFunds(investors[i]);
+            balances[investors[i]] = 0;
+            weiBalances[investors[i]] = 0;
         }
         totalSupply = 0;
     }
 
+    /// @dev Purchasing panel for tokens
+    /// @param _from buyer's address
+    /// @param paidTokens amount of tokens
     function buyPanel(address _from, uint paidTokens) public {
       require(balances[_from] >= paidTokens);
       require(now > icoStage.getEndTime() + 1 years);
@@ -384,6 +455,8 @@ contract Crowdsale is SOL {
       BuyPanels(_from, countPanels);
     }
 
+    /// @dev Exchanging old tokens to new
+    /// @param _amount amount of old tokens
     function exchangeToNewToken(uint _amount) public {
       require(newTokenAddress != 0);
       require(balances[msg.sender] >= _amount);
